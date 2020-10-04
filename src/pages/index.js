e4aad6d2c130f82843b73d708450f6f6ff068830
Tim Chege <mail@timchege.com>
2020-10-04 22:12:26 +0300

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,93 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "../components/layout.css"
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Header from "../components/header"
+import Card from "../components/Card"
+import Section from "../components/Section"
+import staticdata from '../../staticdata.json'
+import Cell from "../components/Cell";
+import styled from 'styled-components'
+
+const SectionGroup = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #94A4BA;
+  text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+  max-width: 600px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    <Header />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about thebest tools and design systems. Protiotype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link>
+        <div className="Logos">
+          <img src={require('../images/logo-sketch.png')} width="50" />
+          <img src={require('../images/logo-figma.png')} width="50" />
+          <img src={require('../images/logo-studio.png')} width="50" />
+          <img src={require('../images/logo-framer.png')} width="50" />
+          <img src={require('../images/logo-react.png')} width="50" />
+          <img src={require('../images/logo-swift.png')} width="50" />
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+    <div className="Cards">
+      <h2>11 courses, more coming</h2>
+      <div className="CardGroup">
+        <Card
+        title="Design System"
+        text="12 sections"
+        image={require('../images/wallpaper.jpg')} />
+        <Card
+        title="React for Designers"
+        text="12 sections"
+        image={require('../images/wallpaper2.jpg')} />
+        <Card
+        title="Sound Design"
+        text="12 sections"
+        image={require('../images/wallpaper3.jpg')} />
+        <Card
+        title="ARKit"
+        text="12 sections"
+        image={require('../images/wallpaper4.jpg')} />
+      </div>
+    </div>
+    <Section
+      image={require('../images/wallpaper2.jpg')}
+      logo={require('../images/logo-react.png')}
+      title="React for Designer"
+      text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
+    />
+    <SectionGroup>12 sections - 6 hours</SectionGroup>
+    <SectionCellGroup>
+      {staticdata.cells.map(cell => (
+        <Cell 
+        title={cell.title} 
+        image={cell.image}/>
+      ))}
+    </SectionCellGroup>
+  </div>
 )
 
 export default IndexPage
